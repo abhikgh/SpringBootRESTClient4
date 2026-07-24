efbd28b8791c5547f07f2ceac7f8ed270f1728c3
Abhik <abhikgh.work@gmail.com>
2026-07-24 07:52:54 +0200

    updates

--- src/main/java/com/example/SpringBootRESTClient4/service/RestClientService.java
+++ src/main/java/com/example/SpringBootRESTClient4/service/RestClientService.java
@@ -41,12 +41,6 @@ public class RestClientService {
                                          @RequestBody OrderRequestForm orderRequestForm) {
         String endpoint = "http://localhost:8092/orders/v1/getOrder4/{month}/{colour}";
 
-     /*   Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("parmRequestSource", parmRequestSource);
-        queryParams.put("parmAudienceType", parmAudienceType);
-        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(endpoint);
-       // queryParams.forEach(uriComponentsBuilder::queryParam);*/
-
         Map<String, String> pathParams = new HashMap<>();
         pathParams.put("month", month);
         pathParams.put("colour", colour);
@@ -55,6 +49,41 @@ public class RestClientService {
                 .fromUriString(endpoint)
                 .queryParam("parmRequestSource", parmRequestSource)
                 .queryParam("parmAudienceType", parmAudienceType)
+                .buildAndExpand(pathParams)
+                .encode()
+                .toUri();
+
+        return
+                restClient
+                        .post()
+                        .uri(uri)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .body(orderRequestForm)
+                        .header("Authorization","Basic YWJoaWtnaDp3ZWxjb21lQDFh")
+                        .header("Actor", "test")
+                        .retrieve()
+                        .body(OrderResponse.class);
+
+    }
+
+    public OrderResponse getOrder43Client(String month, String colour,
+                                          String parmRequestSource,
+                                          String parmAudienceType,
+                                          @RequestBody OrderRequestForm orderRequestForm) {
+        String endpoint = "http://localhost:8092/orders/v1/getOrder4/{month}/{colour}";
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("parmRequestSource", parmRequestSource);
+        queryParams.put("parmAudienceType", parmAudienceType);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(endpoint);
+        queryParams.forEach(uriComponentsBuilder::queryParam);
+
+        Map<String, String> pathParams = new HashMap<>();
+        pathParams.put("month", month);
+        pathParams.put("colour", colour);
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(endpoint)
                 .buildAndExpand(pathParams)
                 .encode()
                 .toUri();
