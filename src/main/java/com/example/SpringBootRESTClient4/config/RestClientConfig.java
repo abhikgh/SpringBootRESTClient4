4b058cb5cf4eaffa5b2a06898a827e2a93c09fae
abhikgh <abhikgh.work@gmail.com>
2026-07-29 16:35:59 +0200

    Update RestClientConfig.java

--- src/main/java/com/example/SpringBootRESTClient4/config/RestClientConfig.java
+++ src/main/java/com/example/SpringBootRESTClient4/config/RestClientConfig.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
 @Configuration
 public class RestClientConfig {
 
