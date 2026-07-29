26838ee71cb392eb47b6fe3daf4cebc44a20042c
abhikgh <abhikgh.work@gmail.com>
2026-07-29 16:34:51 +0200

    Update RestClientConfig.java

--- src/main/java/com/example/SpringBootRESTClient4/config/RestClientConfig.java
+++ src/main/java/com/example/SpringBootRESTClient4/config/RestClientConfig.java
@@ -8,11 +8,22 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(){
-        return RestClient
-                .builder()
+    public RestClient restClient() {
+        return RestClient.builder()
                 .baseUrl("http://localhost:8092/orders/v1/")
+                .requestFactory(requestFactory())
                 .build();
+    }
+    
+    @Bean
+    public ClientHttpRequestFactory requestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+    
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(10));
+        factory.setConnectionRequestTimeout(Duration.ofSeconds(5));
+    
+        return factory;
     }
 
 }
